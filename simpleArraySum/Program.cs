74c562b39d85b90e3c803370af50e3faa900ffc9
KevinPinsan <kevin.pinsan@gmail.com>
2021-01-11 18:02:41 -0300

    Simple array sum solution

---
+++ simpleArraySum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+
+    /*
+     * Complete the simpleArraySum function below.
+     */
+    static int simpleArraySum(int[] ar) {
+       int total = 0;
+       
+       for(int c = 0; c < ar.Length; c++)
+       {
+          total += ar[c]; 
+       }
+       
+       return total;
+    }
+
+    static void Main(string[] args) {
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = simpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+}

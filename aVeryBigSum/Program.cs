348fb281d4da336a38d1fc6d7681c46b724fd6e3
KevinPinsan <kevin.pinsan@gmail.com>
2021-01-13 04:12:35 -0300

    A very big sum solution.

---
+++ aVeryBigSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System;
+
+namespace aVeryBigSum
+{
+    class Solution {
+        static long aVeryBigSum(long[] ar) {
+                long total = 0;
+                
+                for(int c = 0; c < ar.Count(); c++)
+                {
+                    total += ar[c];
+                }         
+                
+                return total;
+        }
+
+        static void Main(string[] args) {
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp))
+            ;
+            long result = aVeryBigSum(ar);
+
+            Console.WriteLine(result);
+        }
+    }
+
+}

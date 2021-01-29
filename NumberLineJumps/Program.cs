080c32d2b6914a7a20544d1da396516f68d6ad56
KevinPinsan <kevin.pinsan@gmail.com>
2021-01-29 18:47:56 -0300

    refatored solution for less code.

--- NumberLineJumps/Program.cs
+++ NumberLineJumps/Program.cs
@@ -12,18 +12,16 @@ using System;
 
 class Solution {
     
-        // Complete the kangaroo function below.
     static string kangaroo(int x1, int v1, int x2, int v2) {
 
-         string result = "";         
+        string result;        
         
         if((x1 < x2 && v1 < v2) || (x2 < x1 && v2 < v1) || (v1 == v2 && x1 != x2)){
             result = "NO";
         }else{
-            if((x2 - x1) % (v2 - v1) == 0) result = "YES";else result = "NO";
+            if((x2 - x1) % (v2 - v1) == 0) result = "YES";
+            else result = "NO";
         }
-
-    
         return result;            
     }
 
